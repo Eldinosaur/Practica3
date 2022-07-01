b508b4343e21d12939f6174d81f419d9b313a84f
Eldinosaur <82054019+Eldinosaur@users.noreply.github.com>
2022-07-01 14:33:49 -0500

    pantalla +1 funcional

--- app/src/main/java/dev/eldinosaur/practica3/MainActivity.kt
+++ app/src/main/java/dev/eldinosaur/practica3/MainActivity.kt
@@ -2,10 +2,23 @@ package dev.eldinosaur.practica3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dev.eldinosaur.practica3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener{
+
+            var numero = binding.textView.text.toString().toInt()
+            numero = numero +1
+            binding.textView.text = numero.toString()
+
+        }
     }
 }
